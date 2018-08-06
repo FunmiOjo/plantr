a0cb5e43a81cefe7bf9cde2f19482dd8ad4fcb04
asyaslutskaya <asyasutskaya@gmail.com>
2018-08-06 17:20:10 -0400

    database is connected

--- models.js
+++ models.js
@@ -1,1 +1,7 @@
-ECHO is on.
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('postgres://localhost:5432/plantr');
+
+// sequelize.authenticate().then(() => {console.log("database is connected");})
+//   .catch(err => {console.log("database is not connected", err)});
+
+module.exports = sequelize;
