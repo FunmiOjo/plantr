f27aba98457182aece7bd22b57749effdc6c0203
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2018-08-06 18:35:01 -0400

    Started creating tables

--- models.js
+++ models.js
@@ -9,11 +9,49 @@ const Gardener = sequelize.define('gardeners', {
   age: Sequelize.INTEGER
 });
 
+const asya = new Gardener({name: 'Asya', age: 30});
+asya.create()
+.then((newRow) => {
+  console.log(newRow);
+})
+.catch((error) => {
+  console.log(error);
+})
+
+const funmi = new Gardener({name: 'Funmi', age: 31});
+funmi.create();
+.then((newRow) => {
+  console.log(newRow);
+})
+.catch((error) {
+  console.log(error);
+});
+
 const Plot = sequelize.define('plots', {
   size: Sequelize.STRING,
   shaded: Sequelize.BOOLEAN
 });
 
+const plot1 = new Plot({size: 'small', shaded: true});
+plot.create()
+.then((newRow) => {
+  console.log(newRow);
+  return newRow
+})
+.then(() => {
+  return plot.setGardener(asya);
+})
+.catch((error) {
+  console.log(error);
+});
+
+const plot2 = new Plot({size: 'medium', shaded: false});
+plot.create()
+.then((newRow) => {
+  console.log(newRow);
+});
+
+plot.setGardener(asya);
 const Vegetable = sequelize.define('vegetables', {
   name: Sequelize.STRING,
   color: Sequelize.STRING,
