708b21c13632a0943c2c408060caf5240916b2e6
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2018-08-06 17:36:35 -0400

    Synced database and defined tables

--- models.js
+++ models.js
@@ -4,4 +4,20 @@ const sequelize = new Sequelize('postgres://localhost:5432/plantr');
 // sequelize.authenticate().then(() => {console.log("database is connected");})
 //   .catch(err => {console.log("database is not connected", err)});
 
+const Gardener = sequelize.define('gardeners', {
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER
+});
+
+const Plot = sequelize.define('plots', {
+  size: Sequelize.STRING,
+  shaded: Sequelize.BOOLEAN
+});
+
+const Vegetable = sequelize.define('vegetables', {
+  name: Sequelize.STRING,
+  color: Sequelize.STRING,
+  planted_on: Sequelize.DATE
+});
+
 module.exports = sequelize;
--- seed.js
+++ seed.js
@@ -1,1 +1,12 @@
+let db = require('./models');
+
+db.sync({force: true})
+.then(() => {
+  console.log('Sync is successful!');
+  db.close();
+})
+.catch(function(error) {
+  console.log(error);
+});
+
 
